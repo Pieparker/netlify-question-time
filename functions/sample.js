706c4805e65428409b66ee372d371d3cc71f2cff
Scott <26013935+Pieparker@users.noreply.github.com>
2020-01-24 21:52:33 +0000

    Update sample.js

--- functions/sample.js
+++ functions/sample.js
@@ -9,7 +9,7 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     helloWorld: (root, args, context) => {
-      return 'This is a demonstrative GraphQL API call.'
+      return `This is a demonstrative GraphQL API call.`
     }
   }
 }
